2a6d678d7202f88f0e0253dee00fc3fee76b3e5b
minseok <71415819+minseokk7@users.noreply.github.com>
2026-07-28 21:23:04 +0900

    Deploy website to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: process.env.GITHUB_PAGES === "true" ? "export" : undefined,
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
